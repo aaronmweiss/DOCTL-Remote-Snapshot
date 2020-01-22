bd88affae35b3e7db639684b463cf08ba390b509
Aaron Weiss <aweissmeister@gmail.com>
2020-01-22 16:56:14 -0600

    February 2020 Sprint - Added snapshot name display for command line use - Improved config file sourcing - Improved snapshot listing to exclude backups from retention - Removed sudo execution requirement until further testing completed

--- auto_snapshot.bash
+++ auto_snapshot.bash
@@ -1,15 +1,34 @@
 #!/bin/bash
-#https://github.com/digitalocean/doctl/issues/309
-exec > >(tee -i do_snapshot.log)
+#exec > >(tee -i do_snapshot.log)
+#set -xv   # this line will enable debug
 
 # Check for root or sudo execution
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root using sudo bash dev-config.bash"
-  exit 1
-fi
+#if [ "$EUID" -ne 0 ]
+#  then echo "Please run as root using sudo bash dev-config.bash"
+#  exit 1
+#fi
+
+# Spinner
+# http://fitnr.com/showing-a-bash-spinner.html
+spinner()
+{
+    local pid=$1
+    local delay=0.3
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+#& spinner $!
 
 # Call dodroplet.config
-. dodroplet.config
+scriptdir=$(dirname "$0")
+source $scriptdir/dodroplet.config
 
 # Weekday, Month, date, YEAR, 12-hour:minute timezone
 DATE=$(date '+%a-%b-%d-%Y@%H:%M-%Z')"$SNAPNAMEAPPEND"
@@ -31,46 +50,47 @@ echo $'\r' >> $email_notification
 echo $'\r' >> $email_notification
 echo $'\r' >> $email_notification
 
-
 echo "Starting script"
 sleep 3
 
 # Shutdown droplet
 echo "Shutting down droplet"
-/snap/bin/doctl compute droplet-action shutdown $DROPLETID --wait
+sudo /snap/bin/doctl compute droplet-action shutdown $DROPLETID --wait
 
 # Create new snapshot using $NAME
-echo "Creating snapshot titled \"$NAME\" value"
+echo "Creating snapshot titled \"$NAME\""
 echo "Please wait this may take awhile. About 1 minute per GB."
-/snap/bin/doctl compute droplet-action snapshot --snapshot-name "$NAME" $DROPLETID --wait
+sudo /snap/bin/doctl compute droplet-action snapshot --snapshot-name "$NAME" $DROPLETID --wait
 
 # Reboot droplet
-echo "Powering on droplet"
-/snap/bin/doctl compute droplet-action power-on $DROPLETID --wait
-echo "We're live baby!"
+echo "Powering on droplet."
+sudo /snap/bin/doctl compute droplet-action power-on $DROPLETID --wait
+echo "Droplet is now powered-on."
 sleep 2
 
 # List snapshots and get oldest snapshots after $NUMRETAIN
-SNAPSHOTS=$(/snap/bin/doctl compute image list-user --format "ID,Type" --no-header | grep snapshot | wc -l)
+SNAPSHOTS=$(sudo /snap/bin/doctl compute image list-user --format "ID,Type" --no-header | grep snapshot | wc -l)
 a=$(($SNAPSHOTS - $NUMRETAIN))
 echo "Deleting the last $a snapshots"
 
 # Deleting all snapshots beyond $NUMRETAIN
-while [ "$SNAPSHOTS" -gt "$NUMRETAIN" ]
+while [[ "$SNAPSHOTS" -gt "$NUMRETAIN" ]]
 	do 
-		OLDEST=$(/snap/bin/doctl compute image list-user --format "ID,Type" --no-header | grep -e '$DROPLETID\|snapshot' | head -n 1)
-		echo yes | /snap/bin/doctl compute image delete $OLDEST --force
-		SNAPSHOTS=$(/snap/bin/doctl compute image list-user --format "ID,Type" --no-header | grep snapshot | wc -l)
+		OLDEST=$(sudo /snap/bin/doctl compute image list-user --format "ID,Type" --no-header | grep -e '$DROPLETID\|snapshot' | awk '{print$1}' | head -n 1)
+		OLDESTNAME=$(sudo /snap/bin/doctl compute snapshot list --format "ID,Name,ResourceId" | grep $DROPLETID | awk '{print$2}' | head -n 1)
+		echo "Deleting "$OLDESTNAME""
+		sudo /snap/bin/doctl compute image delete $OLDEST --force
+		SNAPSHOTS=$(sudo /snap/bin/doctl compute image list-user --format "ID,Type" --no-header | grep snapshot | wc -l)
 done
 sleep 1
 
 #Send email end program
 echo "Sending completion email to $RECIPIENTEMAIL"
 echo "Snapshot of $HOST Created $TODAY"$'\r' >> $email_notification
-sendmail -f "$RECIPIENTEMAIL" $RECIPIENTEMAIL < $email_notification
+sudo sendmail -f "$RECIPIENTEMAIL" $RECIPIENTEMAIL < $email_notification
 
 # Clean up work
-rm -r $email_notification $tmpdir
+sudo rm -r $email_notification $tmpdir
 
 echo "Snapshot Process Completed"
 exit 0
